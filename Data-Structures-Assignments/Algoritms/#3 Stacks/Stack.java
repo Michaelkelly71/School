27b1e25414045b616357e901c2fef673cc9f1837
Michaelkelly71 <64107408+Michaelkelly71@users.noreply.github.com>
2021-03-25 14:02:23 +0000

    Stack Completed

---
+++ Data-Structures-Assignments/Algoritms/#3 Stacks/Stack.java
@@ -0,0 +1,57 @@
+//package com.company;
+public class Stack {
+    public static void main(String args[]){
+		try {
+
+            //pushing Strings elements into stack of size 5
+            System.out.println("\n\t KELLY's STACKS IMPLEMENTATION\n");
+
+
+			StackTest<String> stringStack = new StackTest<String>(5);
+            System.out.println("String list after pushing five books\n");
+			stringStack.push("Physics Book");
+            stringStack.push("Chemistry Book");
+            stringStack.push("Mathematics Book");
+            stringStack.push("Boilogy Book");
+            stringStack.push("English Book");
+
+            //pushing interger elements into stack of size 5
+			StackTest<Integer> integerStack = new StackTest<Integer>(5);
+            System.out.println("\n\nInteger list after pushing five numbers\n");
+
+			integerStack.push(2);
+            integerStack.push(5);
+            integerStack.push(10);
+            integerStack.push(50);
+            integerStack.push(200);
+          
+            
+            System.out.println();//print a line
+            //poping the last string elements out of the stack of size 5
+            stringStack.pop();
+            
+            //poping the last interger elements outta the stack of size 5
+            integerStack.pop();
+
+            System.out.println();//print a line
+
+            System.out.println("String list after popping elements");
+            stringStack.display();//displaying stack list
+
+            System.out.println();//print a line
+
+            System.out.println("\nInteger list after popping elements");
+            integerStack.display();//displaying stack list
+            
+            System.out.println("\n\nPeeks of individual stacks");
+            System.out.println("For String: " + stringStack.peek());
+            
+
+            System.out.println("For Integer: "+ integerStack.peek());
+            
+            
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ Data-Structures-Assignments/Algoritms/#3 Stacks/StackTest.java
@@ -0,0 +1,139 @@
+//package com.company;
+
+public class StackTest<S extends Object> {
+
+    class Node<N> {
+        public N entry;
+        public Node<N> next = null;
+
+    
+        public Node(N entry){
+            this.entry = entry;
+        }
+    }
+
+    private int stackSize;
+    private int elements;
+    private Node<S> top;
+ 
+    /**
+     * constructor to create stack with size
+     * @param size
+     */
+    public StackTest(int size) {
+        stackSize = size;
+        elements = 0;
+        top = null;
+    }
+ 
+
+
+    /**
+     * Adds new entry to the top of the stack
+     * @param entry
+     * @throws Exception 
+     */
+    public void push(S entry) {
+
+        Node<S> newNode = new Node<S>(entry);       //Creates an object of class node with object S
+
+        if(top == null){                            //checks conditions for true to continue or falls to skip 
+            System.out.println("Adding: "+entry);
+            top = newNode;                          //put new node on top as first since there wasn't a node already
+            //elements++;                           //
+            
+        }
+
+        else{
+            System.out.println("Adding: "+entry);
+            newNode.entry = entry;      //assigns the new entry to new node
+            newNode.next = top;         //the next of the top node becomes the new top
+            top = newNode;              //new entry is assigned to the new top created
+            //elements++;
+        }
+        
+    }
+ 
+    /**
+     * Removes an entry from the top of the stack.
+     * @return
+     * @throws Exception 
+     */
+    public S pop() {
+
+        if(isStackEmpty()){
+        	System.out.println("Stack underflow or Empty.");
+            return null;
+        }
+     
+        S popped = top.entry;
+        top = top.next;
+        elements--;
+        
+        System.out.println("Removed entry: " + popped);
+        
+        
+            
+        return popped;
+    }
+ 
+    /**
+     * Returns top of the stack without removing it.
+     * @return
+     */
+    public S peek() {
+        if(!isStackEmpty()){
+            return top.entry;
+            
+        }else{
+            System.out.println("Stack Empty");
+            return null;
+        }
+
+    }
+ 
+    
+    /**
+     * Returns true if the stack is empty
+     * @return
+     */
+    public boolean isStackEmpty() {
+        return (top == null);
+    }
+ 
+    /**
+     * Returns true if the stack is full
+     * @return
+     */
+    public boolean isStackFull() {
+        if (stackSize != getEelemets()){
+            System.out.println("Stack is Full");
+        }
+        return true;
+    }
+
+    public int getEelemets(){
+        return elements;
+    }
+ 
+    public void display()
+    {
+        // check for stack empty
+        if (top == null) {
+            System.out.print("\nStack Empty");
+            return;
+        }
+        else {
+            // create a node and assign top to it
+            Node list = top;
+            // traverse through the elements while displaying their entry
+            while (list != null) {
+
+                // print node entry
+                System.out.println(list.entry);
+                list = list.next;
+            }
+        }
+    }
+	
+}

a7a2b5ed1f1f88a47018eddce40f5f0db26f0a8d
Michaelkelly71 <64107408+Michaelkelly71@users.noreply.github.com>
2021-03-31 19:11:14 +0000

    Update Park.java
    ground

--- Projects/Graphics/A_DAY_AT_THE_AMUSEMENT_PARK/src/Park.java
+++ Projects/Graphics/A_DAY_AT_THE_AMUSEMENT_PARK/src/Park.java
@@ -12,9 +12,6 @@ public class Park extends Applet
 
     private static final long serialVersionUID = 1L;
 
-
-
-
     public  void vertEllipse (int xc, int yc, int r, Graphics g)
     {
         int x0 = 0;
@@ -87,6 +84,37 @@ public class Park extends Applet
         }
     }
 
+    public void groundhEllipse(int centerX,int centerY,int r,Graphics g)
+    {
+        int xo=0;
+        int yo=r;
+
+        int rad=r*2;
+        int d=rad/r;
+
+        int Po=(5/4)-r;
+
+        int xc; xc = centerX;
+        int yc; yc = centerY;
+
+        while(xo<yo)
+        {
+            g.drawString(".",d*(xc-xo),yc+yo);//5th clockwise
+            g.drawString(".",d*(xc-yo),yc+xo);//6th clockwise
+
+            xo++;
+            if(Po<0)
+            {
+                Po=Po+((2*xo)+2)+1;
+            }
+            else
+            {
+                yo--;
+                Po=Po+((2*xo)+2)+1-((2*yo)-2);
+            }
+        }
+    }
+
     public void circle(int x_axis, int y_axis, int radius, Graphics g){
 
         int x0 = 0;
@@ -145,36 +173,98 @@ public class Park extends Applet
         }
     }
 
-    public void positiveLine(int x1, int y1, int x2, int y2,Graphics g)
+    public void positiveLine(int a, int b, int a1, int b1, Graphics g)
     {
-        int dx, dy;
+        int x1=a;
+        int y1=b;
+        int x2=a1;
+        int y2=b1;
+        int dx=x2-x1;
+        int dy=y2-y1;
+        int tdx=-5*dx;
+        int tdy=2*dy;
+        int p=2*dy-dx;
 
-        dx = x2 - x1;
-        dy = y2 - y1;
-
-        int slope = dy / dx;
-
-        for (int i = 0; i <= dy; i++) {
-            g.drawString(".", x1,y1);
-
-            x1++; //increment of steps
-            y1++; //increment of steps
+        for(int k=0;k<dx;k++)
+        {
+            g.drawString(".",x1,y1);
+            if(p<0)
+            {
+                x1++;
+                p=tdy-dx;
+            }
+            else
+            {
+                x1++;
+                y1++;
+                p=p+tdy-tdx;
+            }
         }
     }
 
-    public void negativeLine(int x1, int y1, int x2, int y2,Graphics g)// put pixel for line points
+    public void negativeLine(int a,int b,int a1,int b1,Graphics g)
     {
-        int dx, dy;
+        int x1=a;
+        int y1=b;
+        int x2=a1;
+        int y2=b1;
+        int dx=x2-x1;
+        int dy=y2-y1;
+        int tdy=2*dy;
+        int p=2*dy-dx;
 
-        dx = x2 - x1;
-        dy = y2 - y1;
+        for(int k=0;k<dx;k++)
+        {
+            g.drawString(".",x1,y1);
+            if(p<0)
+            {
+                x1++;
+                y1--;
 
-        for (int i = 0; i < dx; i++) {
-            g.drawString(".", x1,y1);
+                p=p+tdy;
+            }
+            else
+            {
+                x1--;
+                y1++;
 
-            x1--; //increment of steps
-            y1++; //increment of steps
+                p=p+tdy-(2*dx);
+            }
         }
+    }
+
+    public  void string (int xc, int yc, int r, Graphics g)
+    {
+        int x0 = 0;
+        int y0 = r;
+
+        int p0 = (5/4) - r;
+
+        while(x0 <= y0)
+        {
+            //sectors
+            g.drawString(".", xc + x0, 2 * (yc - y0));//1st clockwise
+            g.drawString(".", xc + y0, 2 * (yc - x0));//2nd clockwise
+            g.drawString(".", xc + y0, 2 * (yc + x0));//3rd clockwise
+            g.drawString(".", xc + x0, 2 * (yc + y0));//4th clockwise
+            g.drawString(".", xc - x0, 2 * (yc + y0));//5th clockwise
+            g.drawString(".", xc - y0, 2 * (yc + x0));//6th clockwise
+            g.drawString(".", xc - y0, 2 * (yc - x0));//7th clockwise
+            g.drawString(".", xc - x0, 2 * (yc - y0));//8th clockwise
+
+            x0++;
+            if (p0 < 0)
+            {
+                p0 = p0 + ((2 * x0) + 2) + 1;
+            }
+            else
+            {
+                x0++;
+                y0--;
+                p0 = p0 + ((2 * x0) + 2) + 1 - ((2 * y0) - 2);
+            }
+        }
+
     }
 
 
@@ -185,14 +275,16 @@ public class Park extends Applet
         /**
          * code for sun
          */
-        circle(1319, 40, 50, g);    //Sun
+        circle(1367, 40, 50, g);    //Sun
+
 
         //suns brightness
-        positiveLine(1318,  0,  1348,  40, g);          // 1st line backwards slanted
-        negativeLine(1348,  36,  0,  80, g);            // 2nd line forwards slanted
-        horizontalLine(1320,  75, 1440, g);               // 3rd line flat line
-        positiveLine(1380,  75,  1410,  320, g);        // 4rd line backwards slanted
-        //negativeLine(100,  100,  200,  200, g);         // 5nth line forwards slanted
+        horizontalLine(1280,  2, 1338, g);
+        positiveLine(1280,  2,  1320,  40, g);          // 1st line backwards slanted
+        negativeLine(1320,  40,  1360,  140, g);            // 2nd line forwards slanted
+        horizontalLine(1280,  80, 1340, g);               // 3rd line flat line
+        positiveLine(1340,  81,  1370,  123, g);        // 4rd line backwards slanted
+        negativeLine(1395,  81,  1425,  123, g);         // 5nth line forwards slanted
 
 
 
@@ -201,19 +293,49 @@ public class Park extends Applet
          */
         circle(700, 400, 150, g);                           // wheel's outer circle
         circle(705, 400, 30, g);                            // wheel's inner circle
+        circle(650, 285, 12, g);
+        circle(705, 275, 15, g);
+        circle(760, 285, 12, g);
+
+
+
 
         // Octagon in ferris
-        horizontalLine(700, 300,800,g);
+        horizontalLine(620, 300,780,g);                             //
+        negativeLine(620,300, 670, 400,g);
+        verticalLine(570, 350, 450, g);
+        positiveLine(570,450, 620, 530,g);
+        horizontalLine(620, 500,780,g);
+        positiveLine(780,300, 830, 400,g);
+        verticalLine(830, 350, 450, g);
+        negativeLine(830,450, 880, 550,g);
+
+
 
         // Ferris Stand
-        horizontalLine(685, 400,800,g);             // bar for stand
-        negativeLine(685,400, 800, 800,g);               // left stand outer forwards slanted line
+        horizontalLine(685, 400,725,g);                             // bar for stand
+        negativeLine(685,400, 880, 810,g);                      // left stand outer forwards slanted line
+        positiveLine(725,400, 920, 810, g);
+        negativeLine(705,410, 890, 810,g);
+        positiveLine(705,410, 890, 810, g);
+        horizontalLine(490,595, 520, g);
+        horizontalLine(890,595, 920, g);
+
+        //Ground
+        horizontalLine(150,596,1440, g);
+        groundhEllipse(90,477, 120, g);
 
 
+        //Sea - Saw
+        horizontalLine(190,570,360, g);
+        negativeLine(280,570, 307, 1080,g);
+        positiveLine(280,570, 307, 1080, g);
 
-
-
-
+        //Kite
+        negativeLine(330,170, 360, 1080,g);
+        positiveLine(330,170, 360, 1080, g);
+        negativeLine(360,200, 390, 1080,g);
+        positiveLine(300,200, 330, 1080, g);
 
     }
 }

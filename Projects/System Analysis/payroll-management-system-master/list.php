43ba31fe9d6a1f80dad87424ec43c36db089db63
Michaelkelly71 <64107408+Michaelkelly71@users.noreply.github.com>
2021-04-08 21:19:05 +0000

    Web Done

--- Projects/System Analysis/payroll-management-system-master/list.php
+++ Projects/System Analysis/payroll-management-system-master/list.php
@@ -61,13 +61,13 @@ require 'connection.php';
 
  $result = query( "SELECT * FROM employee ORDER BY id DESC"); // using mysqli_query instead
 	//while($res = mysql_fetch_array($result)) { // mysql_fetch_array is deprecated, we need to use mysqli_fetch_array 
-	while($res = mysqli_fetch_array($result)) { 		
-		echo "<tr>";
-		echo "<td>".$res['name']."</td>";
-		echo "<td>".$res['gender']."</td>";
-		echo "<td>".$res['email']."</td>";	
-		echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";		
-	}
+	// while($res = mysqli_fetch_array($result)) { 		
+	// 	echo "<tr>";
+	// 	echo "<td>".$res['name']."</td>";
+	// 	echo "<td>".$res['gender']."</td>";
+	// 	echo "<td>".$res['email']."</td>";	
+	// 	//echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";		
+	// }
 	
 ?>
 
